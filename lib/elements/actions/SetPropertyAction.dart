6ff0a7827060ea898d8dff04fd86fe5de9df734e
John Evans <prujohn@gmail.com>
2012-03-29 15:30:48 -0500

    property actions system in place and currently has two actions available: AnimationAction & SetPropertyAction

--- lib/elements/actions/SetPropertyAction.dart
+++ lib/elements/actions/SetPropertyAction.dart
@@ -17,11 +17,46 @@
 
 class SetPropertyAction extends ActionBase
 {
+  FrameworkProperty targetProperty;
+  FrameworkProperty propertyProperty;
+  FrameworkProperty valueProperty;
+  
+  
+  SetPropertyAction(){
+    _initSetPropertyActionProperties();
+  }
+  
+  void _initSetPropertyActionProperties(){
+    targetProperty = new FrameworkProperty(this, 'target', (_){});
+    propertyProperty = new FrameworkProperty(this, 'property', (_){});
+    valueProperty = new FrameworkProperty(this, 'value', (_){});
+  }
+  
+  String get target() => getValue(targetProperty);
+  set target(String v) => setValue(targetProperty, v);
+  
+  String get property() => getValue(propertyProperty);
+  set property(String v) => setValue(propertyProperty, v);
+  
+  Dynamic get value() => getValue(valueProperty);
+  set value(Dynamic v) => setValue(valueProperty, v);
   
   BuckshotObject makeMe() => new SetPropertyAction();
   
   void onEventTrigger(){
     
+    //TODO throw?
+    if (target == null || property == null || value == null) return;
+    
+    var el = Buckshot.namedElements[target];
+
+    if (el == null) return; //TODO throw?
+    
+    var prop = el._getPropertyByName(property);
+    
+    if (prop == null) return;
+    
+    setValue(prop, value);
   }
   
   String get type() => "SetPropertyAction";
