4305ab1a1b0ef42a6889d103f120dc3ee32a92bb
John Evans <prujohn@gmail.com>
2012-04-29 21:19:53 -0500

    submodule update.

--- debug/AlignmentPanelTestingView.dart
+++ debug/AlignmentPanelTestingView.dart
@@ -13,12 +13,26 @@ class AlignmentPanelTestingView implements IView {
   
   static final String view = 
 '''
-<border width='300' height='300' background='Orange'>
+<stackpanel>
+<border margin='1' width='300' height='300' background='Orange'>
   <border background='Black' width='50' horizontalalignment='center' verticalalignment='stretch'>
-<textblock horizontalalignment='center' verticalalignment='center' foreground='White'>hi</textblock>
-</border>
+    <textblock horizontalalignment='center' verticalalignment='center' foreground='White'>hi</textblock>
+  </border>
   <!--<textblock horizontalalignment='stretch' verticalalignment='bottom'>hello world</textblock>-->
 </border>
+<border margin='1' width='300' height='300' background='Orange'>
+  <border background='Black' horizontalalignment='stretch' verticalalignment='center'>
+    <textblock horizontalalignment='center' verticalalignment='center' foreground='White'>hi</textblock>
+  </border>
+  <!--<textblock horizontalalignment='stretch' verticalalignment='bottom'>hello world</textblock>-->
+</border>
+<border margin='1' width='300' height='300' background='Orange'>
+  <border background='Black' horizontalalignment='center' verticalalignment='center'>
+    <textblock horizontalalignment='center' verticalalignment='center' foreground='White'>hi</textblock>
+  </border>
+  <!--<textblock horizontalalignment='stretch' verticalalignment='bottom'>hello world</textblock>-->
+</border>
+</stackpanel>
 ''';
   
 }
