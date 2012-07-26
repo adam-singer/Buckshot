b2b2fc6562eef61861eee131c984b8142b8589d1
John Evans <prujohn@gmail.com>
2012-07-26 11:07:39 -0500

    sketched in platform handlers

--- lib/core/Polly.dart
+++ lib/core/Polly.dart
@@ -36,7 +36,6 @@ class Polly {
       throw const BuckshotException('Unable to declare box model on element.');
     }
   }
-  
 
   /** Returns a string representing a cross-browser CSS property assignment. */
   static String generateXPCSS(String declaration, String value){
@@ -71,7 +70,7 @@ class Polly {
   /** Removes a given CSS property from an HTML element. Supports all common browser prefixes. */
   static void removeXPCSS(Element e, String property){
     for(final String p in prefixes){
-      var pre = '${p}${property}'; //assigning here because some bug won't let me pass it directly in .setProperty
+      var pre = '${p}${property}'; //assigning here because some bug won't let me pass it directly in .removeProperty
       e.style.removeProperty(pre);
     }
   }
@@ -94,7 +93,7 @@ class Polly {
   static String getXPCSS(Element e, String property){
 
     for(final String p in prefixes){
-      var pre = '${p}${property}'; //assigning here because some bug won't let me pass it directly in .setProperty
+      var pre = '${p}${property}'; //assigning here because some bug won't let me pass it directly in .getProperty
       String result = e.style.getPropertyValue(pre);
 
       if (result != null) return result;
@@ -106,94 +105,44 @@ class Polly {
   static void setFlexBoxOrientation(Element element, Orientation orientation){
     setXPCSS(element, 'flex-direction', (orientation == Orientation.horizontal) ? 'row' : 'column');
   }
-
   /// For individual items within a flexbox, but only in the cross-axis.
   static void setHorizontalItemFlexAlignment(Element element, HorizontalAlignment alignment){
-    switch(alignment){
-      case HorizontalAlignment.left:
-        setXPCSS(element, 'align-self', 'flex-start');
-        break;
-      case HorizontalAlignment.right:
-        setXPCSS(element, 'align-self', 'flex-end');
-        break;
-      case HorizontalAlignment.center:
-        setXPCSS(element, 'align-self', 'center');
-        break;
-      case HorizontalAlignment.stretch:
-        setXPCSS(element, 'align-self', 'stretch');
-        break;
-      }
+    if (_platform == null) _setPlatform();
+    _platform.setHorizontalItemFlexAlignment(element, alignment);
   }
 
   /// For individual items within a flexbox, but only in the cross-axis.
   static void setVerticalItemFlexAlignment(Element element, VerticalAlignment alignment){
-
-    switch(alignment){
-      case VerticalAlignment.top:
-        setXPCSS(element, 'align-self', 'flex-start');
-        break;
-      case VerticalAlignment.bottom:
-        setXPCSS(element, 'align-self', 'flex-end');
-        break;
-      case VerticalAlignment.center:
-        setXPCSS(element, 'align-self', 'center');
-        break;
-      case VerticalAlignment.stretch:
-        setXPCSS(element, 'align-self', 'stretch');
-        break;
-      }
+    if (_platform == null) _setPlatform();
+    _platform.setVerticalItemFlexAlignment(element, alignment);
   }
 
   static void setHorizontalFlexBoxAlignment(Element element, HorizontalAlignment alignment){
-    switch(alignment){
-      case HorizontalAlignment.left:
-        setXPCSS(element, 'justify-content', 'flex-start');
-        //element._component.style.flexPack = 'start';
-        break;
-      case HorizontalAlignment.right:
-        setXPCSS(element, 'justify-content', 'flex-end');
-        //element._component.style.flexPack = 'end';
-        break;
-      case HorizontalAlignment.center:
-        setXPCSS(element, 'justify-content', 'center');
-        //element._component.style.flexPack = 'center';
-        break;
-      case HorizontalAlignment.stretch:
-        //TODO start?
-        setXPCSS(element, 'justify-content', 'stretch');
-        //element._component.style.flexPack = 'start';
-        break;
-      }
+    if (_platform == null) _setPlatform();
+    _platform.setHorizontalFlexBoxAlignment(element, alignment);
   }
 
   static void setVerticalFlexBoxAlignment(Element element, VerticalAlignment alignment){
-    switch(alignment){
-      case VerticalAlignment.top:
-        setXPCSS(element, 'align-items', 'flex-start');
-        //element._component.style.flexAlign = 'start';
-        break;
-      case VerticalAlignment.bottom:
-        setXPCSS(element, 'align-items', 'flex-end');
-        //element._component.style.flexAlign = 'end';
-        break;
-      case VerticalAlignment.center:
-        setXPCSS(element, 'align-items', 'center');
-        //element._component.style.flexAlign = 'center';
-        break;
-      case VerticalAlignment.stretch:
-        setXPCSS(element, 'align-items', 'stretch');
-        //element._component.style.flexAlign = 'stretch';
-        break;
-    }
+    if (_platform == null) _setPlatform();
+    _platform.setVerticalFlexBoxAlignment(element, alignment);
+  } 
+  
+  /**
+   * Sets the alignment of a given [element] within it's parent 
+   * [FrameworkElement].  This function assumes that the parent element
+   * is already flexbox.
+   */
+  static void setFlexboxAlignment(FrameworkElement element){
+    if (_platform == null) _setPlatform();
+    _platform.setFlexboxAlignment(element);
   }
   
+  /* Internal */
   
   static _BrowserImpl _platform;
   
-  static void setPlatform()
-  {
-    if (_platform != null) return;
-    
+  static void _setPlatform()
+  {    
     switch(browserInfo.browser){
       case 'IE':
         _platform = new _IE();
@@ -217,72 +166,146 @@ class Polly {
         _platform = new _Safari();
         break;
       default:
-        _platform = new _Chrome();
+        _platform = new _BrowserImpl();
         break;
     }
   }
-  
-  /**
-   * Sets the alignment of a given [element] within it's parent 
-   * [FrameworkElement].  This function assumes that the parent element
-   * is already flexbox.
-   */
-  static void setFlexboxAlignment(FrameworkElement element){
-    setXPCSS(element.rawElement, 'flex', 'none');
+}
+
+
+class _BrowserImpl
+{
+  void setFlexboxAlignment(FrameworkElement element){
+    
+    Polly.setXPCSS(element.rawElement, 'flex', 'none');
     
     if (element.hAlign != null){
       if(element.hAlign == HorizontalAlignment.stretch){
-        setXPCSS(element.rawElement, 'flex', '1 1 auto');
+        Polly.setXPCSS(element.rawElement, 'flex', '1 1 auto');
       }
       
-      setHorizontalFlexBoxAlignment(element.parent.rawElement, element.hAlign);
+      Polly.setHorizontalFlexBoxAlignment(element.parent.rawElement, element.hAlign);
     }
 
     if (element.vAlign != null){
-      setVerticalFlexBoxAlignment(element.parent.rawElement, element.vAlign);
+      Polly.setVerticalFlexBoxAlignment(element.parent.rawElement, element.vAlign);
+    }
+  }
+  
+  void setHorizontalItemFlexAlignment(Element element, HorizontalAlignment alignment){
+    switch(alignment){
+      case HorizontalAlignment.left:
+        Polly.setXPCSS(element, 'align-self', 'flex-start');
+        break;
+      case HorizontalAlignment.right:
+        Polly.setXPCSS(element, 'align-self', 'flex-end');
+        break;
+      case HorizontalAlignment.center:
+        Polly.setXPCSS(element, 'align-self', 'center');
+        break;
+      case HorizontalAlignment.stretch:
+        Polly.setXPCSS(element, 'align-self', 'stretch');
+        break;
+      }
+  }
+
+  void setVerticalItemFlexAlignment(Element element, VerticalAlignment alignment){
+
+    switch(alignment){
+      case VerticalAlignment.top:
+        Polly.setXPCSS(element, 'align-self', 'flex-start');
+        break;
+      case VerticalAlignment.bottom:
+        Polly.setXPCSS(element, 'align-self', 'flex-end');
+        break;
+      case VerticalAlignment.center:
+        Polly.setXPCSS(element, 'align-self', 'center');
+        break;
+      case VerticalAlignment.stretch:
+        Polly.setXPCSS(element, 'align-self', 'stretch');
+        break;
+      }
+  }
+
+  void setHorizontalFlexBoxAlignment(Element element, HorizontalAlignment alignment){
+    switch(alignment){
+      case HorizontalAlignment.left:
+        Polly.setXPCSS(element, 'justify-content', 'flex-start');
+        //element._component.style.flexPack = 'start';
+        break;
+      case HorizontalAlignment.right:
+        Polly.setXPCSS(element, 'justify-content', 'flex-end');
+        //element._component.style.flexPack = 'end';
+        break;
+      case HorizontalAlignment.center:
+        Polly.setXPCSS(element, 'justify-content', 'center');
+        //element._component.style.flexPack = 'center';
+        break;
+      case HorizontalAlignment.stretch:
+        //TODO start?
+        Polly.setXPCSS(element, 'justify-content', 'stretch');
+        //element._component.style.flexPack = 'start';
+        break;
+      }
+  }
+
+  void setVerticalFlexBoxAlignment(Element element, VerticalAlignment alignment){
+    switch(alignment){
+      case VerticalAlignment.top:
+        Polly.setXPCSS(element, 'align-items', 'flex-start');
+        //element._component.style.flexAlign = 'start';
+        break;
+      case VerticalAlignment.bottom:
+        Polly.setXPCSS(element, 'align-items', 'flex-end');
+        //element._component.style.flexAlign = 'end';
+        break;
+      case VerticalAlignment.center:
+        Polly.setXPCSS(element, 'align-items', 'center');
+        //element._component.style.flexAlign = 'center';
+        break;
+      case VerticalAlignment.stretch:
+        Polly.setXPCSS(element, 'align-items', 'stretch');
+        //element._component.style.flexAlign = 'stretch';
+        break;
     }
   }
 }
 
-class _Chrome implements _BrowserImpl
+class _Chrome extends _BrowserImpl
 {
 
 }
 
-class _Mozilla implements _BrowserImpl
+class _Mozilla extends _BrowserImpl
 {
 
 }
 
-class _IE implements _BrowserImpl
-{
-  
-}
-
-class _Android implements _BrowserImpl
+class _IE extends _BrowserImpl
 {
   
 }
 
-class _Chromium implements _BrowserImpl
+class _Android extends _BrowserImpl
 {
   
 }
 
-class _Opera implements _BrowserImpl
+class _Chromium extends _BrowserImpl
 {
   
 }
 
-class _Safari implements _BrowserImpl
+class _Opera extends _BrowserImpl
 {
   
 }
 
-class _BrowserImpl
+class _Safari extends _BrowserImpl
 {
-
+  
 }
+
 
 class FlexModel{
   final int _val;
