9824883e8a2637d092c777594a2648f601ff7056
John Evans <prujohn@gmail.com>
2012-08-23 14:41:52 -0500

    cleanup.

--- lib/core/FrameworkPropertyBase.dart
+++ lib/core/FrameworkPropertyBase.dart
@@ -9,30 +9,32 @@
 * ## See Also
 * * [FrameworkProperty]
 * * [AttachedFrameworkProperty]
+* * [AnimatingFrameworkProperty]
 */
-class FrameworkPropertyBase extends HashableObject{
-  /// Holds a reference to the object that the property belongs to.
+class FrameworkPropertyBase extends HashableObject
+{
+  /** Holds a reference to the object that the property belongs to. */
   final BuckshotObject sourceObject;
-  /// Holds a callback function that is invoked whenever the value
-  /// of a property changes.
+
+  /**
+   * Holds a callback function that is invoked whenever the value
+   * of a property changes.
+   */
   final Function propertyChangedCallback;
-  /// Holds the friendly name of the property.
+
+  /** Holds the friendly name of the property. */
   final String propertyName;
-  /// Fires when the property value changes.
+
+  /** Fires when the property value changes. */
   final FrameworkEvent<PropertyChangingEventArgs> propertyChanging;
-  /// Holds a converter that is used to convert strings into the type
-  /// Required by the property.
+
+  /**
+   * Holds a converter that is used to convert strings into the type
+   * required by the property.
+   */
   final IValueConverter stringToValueConverter;
 
-  /// Constructs a FrameworkPropertyBase and initializes it to the framework.
-  ///
-  /// ### Parameters
-  /// * [LucaObject] sourceObject - the object the property belongs to.
-  /// * [String] propertyName - the friendly public name for the property.
-  /// * [Function] propertyChangedCallback - called by the framework when the property value changes.
   FrameworkPropertyBase(this.sourceObject, this.propertyName, callback, [this.stringToValueConverter = null]) :
    propertyChanging = new FrameworkEvent<PropertyChangingEventArgs>(),
    propertyChangedCallback = (callback == null ? (_){} : callback);
-//
-//   String toString() => '(${sourceObject}) $propertyName';
 }
