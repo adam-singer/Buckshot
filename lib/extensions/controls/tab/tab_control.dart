eb358daa011ccb2a0e0d3ba241bc0430204f3a77
John Evans <prujohn@gmail.com>
2012-09-11 14:36:24 -0500

    more progress on TabControl. control is usable and provides some customization.

--- lib/extensions/controls/tab/tab_control.dart
+++ lib/extensions/controls/tab/tab_control.dart
@@ -19,11 +19,18 @@ class TabControl extends Control implements IFrameworkContainer
   FrameworkProperty tabItemsProperty;
   FrameworkProperty tabAlignmentProperty;
   FrameworkProperty tabBackgroundProperty;
+  FrameworkProperty tabSelectedBrushProperty;
+  FrameworkProperty backgroundProperty;
 
   final FrameworkEvent<TabSelectedEventArgs> tabSelected;
+  final FrameworkEvent<TabSelectedEventArgs> tabClosing;
+  
+  TabItem currentTab;
+  Brush _tabBackground;
   
   TabControl() :
-    tabSelected = new FrameworkEvent<TabSelectedEventArgs>()
+    tabSelected = new FrameworkEvent<TabSelectedEventArgs>(),
+    tabClosing = new FrameworkEvent<TabSelectedEventArgs>()
   {
     Browser.appendClass(rawElement, "TabControl");
 
@@ -32,18 +39,64 @@ class TabControl extends Control implements IFrameworkContainer
     stateBag[FrameworkObject.CONTAINER_CONTEXT] = getValue(tabItemsProperty);
 
     loaded + _initControl;
-    
-    tabSelected + (_, args){
-      print('selected ${getValue(args.tab.headerProperty)}');
-    };
   }
 
   TabControl.register() :
-    tabSelected = new FrameworkEvent<TabSelectedEventArgs>();
+    tabSelected = new FrameworkEvent<TabSelectedEventArgs>(),
+    tabClosing = new FrameworkEvent<TabSelectedEventArgs>();
   makeMe() => new TabControl();
 
   get content => getValue(tabItemsProperty);
 
+  void switchToTab(TabItem tab){
+    if (currentTab == tab) return;
+    
+    if (currentTab != null){
+      
+      //remove active markings on this tab.
+      setValue(currentTab.closeButtonVisiblityProperty, Visibility.collapsed);
+      
+      currentTab._visualTemplate.dynamic.background = _tabBackground;
+      
+      currentTab._visualTemplate.dynamic.borderThickness = 
+          new Thickness.specified(1, 1, 0, 1);
+    }
+    
+//    print('switch to ${getValue(tab.headerProperty)}');
+    currentTab = tab;
+    
+    final t = currentTab._visualTemplate;
+      //set active markings on the tab.
+      
+    _tabBackground = getValue(t.dynamic.backgroundProperty);
+    
+    setValue(currentTab.closeButtonVisiblityProperty, Visibility.visible);
+    
+    currentTab._visualTemplate.dynamic.borderThickness = 
+        new Thickness.specified(2, 2, 0, 2);
+    
+    setValue(t.dynamic.backgroundProperty, 
+        getValue(tabSelectedBrushProperty));
+    
+    setValue(currentContentProperty, 
+        getValue(currentTab.contentProperty));
+
+  }
+
+  void closeTab(TabItem tab){
+    //TODO add handling for last tab closed.
+    if (tabItems.length == 1) return;
+      
+    tab._visualTemplate.rawElement.remove();
+    
+    tabItems.removeRange(tabItems.indexOf(tab), 1);
+    
+    currentTab = null;
+    
+    switchToTab(tabItems[0]);
+    
+  }
+  
   
   void _initControl(sender, args){
     if (tabItems.isEmpty()) return;
@@ -58,12 +111,28 @@ class TabControl extends Control implements IFrameworkContainer
     int i = 0;
     pc.forEach((e){
       final ti = tabItems[i++];
-      e.click + (_, __){
+      ti._visualTemplate = e;
+      e.mouseUp + (_, __){
         tabSelected.invokeAsync(this, new TabSelectedEventArgs(ti));
       };
+      
+      final b = Template.findByName('__close_button__', e);
+      assert(b != null);
+      
+      b.mouseUp + (_, __){
+        tabClosing.invokeAsync(this, new TabSelectedEventArgs(ti));
+      };
     });
-    assert(pc != null);
+
+    tabSelected + (_, args){
+      switchToTab(args.tab);
+    };
     
+    tabClosing + (_, args){
+      closeTab(args.tab);
+    };
+    
+    switchToTab(tabItems[0]);
     
   }
   
@@ -71,15 +140,23 @@ class TabControl extends Control implements IFrameworkContainer
     currentContentProperty = new FrameworkProperty(this, 'currentContent');
 
     tabItemsProperty = new FrameworkProperty(this, 'tabItems',
-        defaultValue: new ObservableList<TabItem>());
+        defaultValue: new List<TabItem>());
 
     tabAlignmentProperty = new FrameworkProperty(this, 'tabAlignment',
         defaultValue: HorizontalAlignment.left,
         converter: const StringToHorizontalAlignmentConverter());
     
+    tabSelectedBrushProperty = new FrameworkProperty(this, 'tabSelectedBrush',
+        defaultValue: new SolidColorBrush(new Color.predefined(Colors.White)),
+        converter: const StringToSolidColorBrushConverter());
+    
+    backgroundProperty = new FrameworkProperty(this, 'background',
+        defaultValue: new SolidColorBrush(new Color.predefined(Colors.White)),
+        converter: const StringToSolidColorBrushConverter());
+    
   }
 
-  ObservableList<TabItem> get tabItems => getValue(tabItemsProperty);
+  List<TabItem> get tabItems => getValue(tabItemsProperty);
   
 
   String get defaultControlTemplate {
@@ -87,27 +164,39 @@ class TabControl extends Control implements IFrameworkContainer
 '''
 <controltemplate controlType='${this.templateName}'>
   <template>
-     <stack>
+     <grid valign='{template vAlign}' halign='{template hAlign}' height='{template height}' width='{template width}'>
+        <rowdefinitions>
+           <rowdefinition height='auto' />
+           <rowdefinition height='*' />
+        </rowdefinitions>
         <collectionpresenter name='__tc_presenter__' halign='{template tabAlignment}' collection='{template tabItems}'>
            <presentationpanel>
               <stack orientation='horizontal' />
            </presentationpanel>
            <itemstemplate>
-              <border cornerradius='7,7,0,0' cursor='Arrow' background='White' borderthickness='1,1,0,1' bordercolor='Black' padding='2'>
+              <border valign='stretch' cornerradius='7,7,0,0' cursor='Arrow' background='WhiteSmoke' borderthickness='1,1,0,1' bordercolor='Gray' padding='2'>
                  <stack orientation='horizontal'>
                     <contentpresenter content='{data icon}' margin='0,2,0,0' />
                     <contentpresenter content='{data header}' margin='0,3,0,0' />
-                    <border width='25'>
-                      <button visibility='{data closeButtonVisibility}' content='x' />
+                    <border margin='0,2,0,3' valign='center' width='12' height='12'>
+                      <border name='__close_button__' borderColor='Gray' borderthickness='1' halign='stretch' valign='stretch' visibility='{data closeButtonVisibility}'>
+                          <actions>
+                             <setproperty event='mouseEnter' property='background' value='Orange' />
+                             <setproperty event='mouseLeave' property='background' value='White' />
+                             <setproperty event='mouseDown' property='background' value='#CCCCCC' />
+                             <setproperty event='mouseUp' property='background' value='Orange' />
+                          </actions>
+                          <textblock text='X' valign='center' halign='center' fontfamily='Arial' fontsize='10' />
+                      </border>
                     </border>
                  </stack>
               </border>
            </itemstemplate>
         </collectionpresenter>
-        <border halign='stretch' background='WhiteSmoke' borderthickness='1' 
-             bordercolor='Black' padding='5' content='{template currentContent}'>
+        <border grid.row='1' halign='stretch' bordercolor='Gray' borderthickness='1' valign='stretch' background='{template background}' padding='5'>
+            <contentpresenter content='{template currentContent}' />
         </border>
-     </stack>
+     </grid>
   </template>
 </controltemplate>
 ''';
--- lib/extensions/controls/tab/tab_item.dart
+++ lib/extensions/controls/tab/tab_item.dart
@@ -2,28 +2,55 @@
 // https://github.com/prujohn/Buckshot
 // See LICENSE file for Apache 2.0 licensing information.
 
-class TabItem extends Control
+class TabItem extends Control implements IFrameworkContainer
 {
   FrameworkProperty headerProperty;
   FrameworkProperty iconProperty;
   FrameworkProperty closeEnabledProperty;
   FrameworkProperty closeButtonVisiblityProperty;
+  FrameworkProperty contentProperty;
+  
+  FrameworkElement _visualTemplate;
   
   TabItem(){
     Browser.appendClass(rawElement, "TabItem"); 
-    
+        
     _initTabItemProperties();
+    
+    stateBag[FrameworkObject.CONTAINER_CONTEXT] = contentProperty;
   }
    
   TabItem.register();
   makeMe() => new TabItem();
   
+  get content => getValue(contentProperty);
+  
   void _initTabItemProperties(){
     headerProperty = new FrameworkProperty(this, 'header');
+    
     iconProperty = new FrameworkProperty(this, 'icon');
+    
+    contentProperty = new FrameworkProperty(this, 'content', 
+        defaultValue: 'hello johnny');
+    
     closeButtonVisiblityProperty = new FrameworkProperty(this, 
         'closeButtonVisibility',
+        propertyChangedCallback: (Visibility value){
+          if (value == Visibility.visible 
+              && getValue(closeEnabledProperty) == false){
+            setValue(closeButtonVisiblityProperty, Visibility.collapsed);
+          }
+        },
         defaultValue: Visibility.collapsed,
         converter: const StringToVisibilityConverter());
+    
+    closeEnabledProperty = new FrameworkProperty(this, 'closeEnabled',
+        propertyChangedCallback: (bool value){
+          if (value == false){
+            setValue(closeButtonVisiblityProperty, Visibility.collapsed);          
+          }
+        },
+        defaultValue: true,
+        converter: const StringToBooleanConverter());
   }
 }
