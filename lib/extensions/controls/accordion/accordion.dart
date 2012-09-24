bc07fc4d5ab81776d464db1e735a39bba8ad89e3
John Evans <prujohn@gmail.com>
2012-09-24 09:42:10 -0500

    accordion layout works in templates, ref #47

--- lib/extensions/controls/accordion/accordion.dart
+++ lib/extensions/controls/accordion/accordion.dart
@@ -15,30 +15,48 @@
 class Accordion extends Control implements IFrameworkContainer
 {
   
+  FrameworkProperty accordionItemsProperty;
+  FrameworkProperty backgroundProperty;
+  
   Accordion()
   {
     Browser.appendClass(rawElement, "Accordion");
+    
+    _initializeAccordianProperties();
+    
+    stateBag[FrameworkObject.CONTAINER_CONTEXT] = 
+        getValue(accordionItemsProperty);
   }
 
   Accordion.register() : super.register(){
     buckshot.registerElement(new AccordionItem.register());
   }
   makeMe() => new Accordion();
-  
-  
-  
-  
+    
+  void _initializeAccordianProperties(){
+    accordionItemsProperty = new FrameworkProperty(this, 'accordionItems', 
+        defaultValue: new List<FrameworkObject>());
+    
+    backgroundProperty = new FrameworkProperty(this, 'background',
+        defaultValue: new SolidColorBrush(new Color.predefined(Colors.White)),
+        converter: const StringToSolidColorBrushConverter());
+  }
+    
+  get content => getValue(accordionItemsProperty);
+    
   String get defaultControlTemplate {
     return
 '''
 <controltemplate controlType='${this.templateName}'>
   <template>
-    <border valign='{template vAlign}' halign='{template hAlign}' height='{template height}' width='{template width}'>
-      <collectionpresenter name='__ac_presenter__' collection='{template accordionItems}'>
+    <border background='{template background}' valign='{template vAlign}' 
+            halign='{template hAlign}' height='{template height}' 
+            width='{template width}'>
+      <collectionpresenter halign='stretch' name='__ac_presenter__' collection='{template accordionItems}'>
          <itemstemplate>
-            <stack>
-               <contentpresenter content='{data header}' />
-               <contentpresenter content='{data body}' />
+            <stack halign='stretch'>
+               <contentpresenter halign='stretch' content='{data header}' />
+               <contentpresenter halign='stretch' content='{data body}' />
             </stack>
          </itemstemplate>
       </collectionpresenter>
--- lib/extensions/controls/accordion/accordion_item.dart
+++ lib/extensions/controls/accordion/accordion_item.dart
@@ -1,11 +1,25 @@
 
 class AccordionItem extends Control implements IFrameworkContainer 
 {
+  FrameworkProperty headerProperty;
+  FrameworkProperty bodyProperty;
+    
   AccordionItem()
   {
     Browser.appendClass(rawElement, "AccordionItem");
+    
+    _initAccordionItemProperties();
+    
+    stateBag[FrameworkObject.CONTAINER_CONTEXT] = bodyProperty;
   }
 
   AccordionItem.register() : super.register();
   makeMe() => new AccordionItem();
+  
+  get content => getValue(bodyProperty);
+  
+  void _initAccordionItemProperties(){
+    headerProperty = new FrameworkProperty(this, 'header');
+    bodyProperty = new FrameworkProperty(this, 'body');
+  } 
 }
